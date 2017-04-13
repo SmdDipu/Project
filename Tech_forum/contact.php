a12d662addcab0a9a448d4a0f15daef47d238f56
Smd Dipu <shahjahan_dipu@diu.edu.bd>
2017-04-13 13:25:33 +0600

    Online Forum

---
+++ Tech_forum/contact.php
@@ -0,0 +1,17 @@
+<?php  require("header.php"); ?>
+<script type="text/javascript">
+	document.getElementById("acontact").className="active";
+</script>
+
+
+	<h1>Contact Us</h1>
+    <table>
+    
+	
+    <tr><td rowspan="4"><img src="ups/sd.jpg" style='height:100px; width:100px';></td><td>Name</td><td>:</td><td>TechForum</td></tr>
+    <tr><td rowspan="1">Contact No</td><td>:</td><td>0123456789</td></tr>
+    <tr><td rowspan="1">E-Mail</td><td>:</td><td>techforum@hotmail.net</td></tr>
+   <tr><td rowspan="1">Website</td><td>:</td><td><a href="#">www.techforum.com</a></td></tr>
+    </table>
+    
+<?php require("footer.php"); ?>
